747874fba539f8e0fc1c69f5e45f29bd5e30f93e
Andoner <Andsmith316@gmail.com>
2022-04-02 23:23:12 -0700

    intial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+ constructor() {
+    super();
+    this.state = {
+      name: "Dino"
+    }
+ }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Helllo {this.state.name}</p>
+          <button onClick={() => {
+            
+          }}>Change name</button>
+        </header>
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
